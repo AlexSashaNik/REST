198008f26794f113a9b856bc668b7f315f9dce88
AlexNikitina <alex.nikitina82@gmail.com>
2023-08-10 17:33:20 +0200

    Enum added in schema.json

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -12,10 +12,10 @@ class MobileBankApiTestV1 {
       given()
           .baseUri("http://localhost:9999/api/v1")
       // Выполняемые действия
-      .when()
+          .when()
           .get("/demo/accounts")
       // Проверки
-      .then()
+          .then()
           .statusCode(200);
     }
 }
